e4596831c49c9ad68b7a73a577748fc1d505c6d3
mmargauxx <margaux.laurier@gmail.com>
2020-12-14 00:55:46 +0100

    changes to names

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,12 +2,11 @@ import React from "react";
 import BaseList from "./BaseList";
 import Instructions from "./Instructions";
 
-function Pokemon({ name, ingredients, steps }) {
+function Pokemon({ name, base }) {
   return (
     <section id={name.toLowerCase().replace(/ /g, "-")}>
       <h1>{name}</h1>
       <BaseList list={base} />
-      <Instructions title="Cooking Instructions" steps={steps} />
     </section>
   );
 }
