fc0e89fef63a9dbf23e7bd6f366cc0a6e498087f
mmargauxx <margaux.laurier@gmail.com>
2020-12-14 00:00:44 +0100

    commit zalm

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ function Menu({ recipes }) {
   return (
     <article>
       <header>
-        <h1>Delicious Recipes</h1>
+        <h1>Favorite Artists</h1>
       </header>
       <div className="recipes">
         {recipes.map((recipe, i) => (
